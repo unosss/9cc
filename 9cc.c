1362243965c9f4d5627f4f9a4ef575ee3b66dd53
unyochi <unofamil@outlook.com>
2024-04-14 01:40:33 +0900

    再帰下降構文解析による四則演算言語のコンパイル

--- 9cc.c
+++ 9cc.c
@@ -102,7 +102,7 @@ Token *tokenize(char *p) {
 			continue;
 		}
 
-		if (*p == '+' || *p == '-') {
+		if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')') {
 			cur = new_token(TK_RESERVED, cur, p++);
 			continue;
 		}
@@ -122,7 +122,7 @@ Token *tokenize(char *p) {
 
 // 抽象構文木のノードの種類
 typedef enum {
-	ND_ADD, // +
+	ND_ADD,  // +
 	ND_SUB,	 // -
 	ND_MUL,	 // *
 	ND_DIV,	 // /
@@ -153,6 +153,9 @@ Node *new_node_num(int val){
 	node->val = val;
 	return node;
 }
+
+Node *mul();
+Node *primary();
 
 Node *expr(){
 	Node *node = mul();
@@ -195,6 +198,7 @@ Node *primary(){
 void gen(Node *node){
 	if(node->kind == ND_NUM){
 		printf("	push %d\n", node->val);
+		return;
 	}
 	gen(node->lhs);
 	gen(node->rhs);
@@ -207,14 +211,14 @@ void gen(Node *node){
 		printf("	add rax, rdi\n");
 		break;
 	case ND_SUB:
-		printf("	sub rax, rdi\n"):
+		printf("	sub rax, rdi\n");
 		break;
 	case ND_MUL:
 		printf("	imul rax, rdi\n");
 		break;
 	case ND_DIV:
 		printf("	cqo\n");
-		printf("	idiv rid\n");
+		printf("	idiv rdi\n");
 		break;
 	}
 	printf("	push rax\n");
@@ -227,31 +231,21 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
-// トークナイズする
+// トークナイズしてパースする
 	user_input = argv[1];
-	token = tokenize(argv[1]);
-
+	token = tokenize(user_input);
+	Node *node = expr();
 // アセンブリの前半部分を出力
 	printf(".intel_syntax noprefix\n");
 	printf(".globl main\n");
 	printf("main:\n");
+	
+// 抽象構文木を下りながらコード生成
+	gen(node);
 
-// 式の最初は数でなければならないので、それをチェックして
-// 最初のmov命令を出力
-	printf("  mov rax, %d\n", expect_number());
-
-// `+ <数>`あるいは`- <数>`というトークンの並びを消費しつつ
-// アセンブリを出力
-	while (!at_eof()) {
-		if (consume('+')) {
-			printf("	add rax, %d\n", expect_number());
-			continue;
-		}
-
-		expect('-');
-		printf("	sub rax, %d\n", expect_number());
-	}
-
+// スタックトップに式全体の値が残っているはずなので
+// それをRAXにロードして関数からの返り値とする
+	printf("	pop rax\n");
 	printf("	ret\n");
 	return 0;
 }
