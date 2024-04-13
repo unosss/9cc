2459bec9e7ab1d505cd58f62824ebaf848445191
unyochi <unofamil@outlook.com>
2024-04-14 02:00:45 +0900

    単項演算子を追加

--- 9cc.c
+++ 9cc.c
@@ -156,6 +156,8 @@ Node *new_node_num(int val){
 
 Node *mul();
 Node *primary();
+Node *unary();
+
 
 Node *expr(){
 	Node *node = mul();
@@ -171,13 +173,13 @@ Node *expr(){
 }
 
 Node *mul(){
-	Node *node = primary();
+	Node *node = unary();
 
 	for (;;){
 		if(consume('*'))
-			node = new_node(ND_MUL, node, primary());
+			node = new_node(ND_MUL, node, unary());
 		else if(consume('/'))
-			node = new_node(ND_DIV, node, primary());
+			node = new_node(ND_DIV, node, unary());
 		else
 			return node;
 	}
@@ -193,6 +195,14 @@ Node *primary(){
 
 	// そうでなければ数値のはず
 	return new_node_num(expect_number());
+}
+
+Node *unary() {
+	if (consume('+'))
+		return primary();
+	if (consume('-'))
+		return new_node(ND_SUB, new_node_num(0), primary());
+	return primary();
 }
 
 void gen(Node *node){
