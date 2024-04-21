2446f6cd61dec820d76ddadf5912acf8da055ddf
unosss <unofamil@outlook.com>
2024-04-22 05:35:46 +0900

    nodeの実装をparse.cに寄せた

--- parse.c
+++ parse.c
@@ -7,6 +7,19 @@
 #include <string.h>
 
 
+char *ADD = "+";
+char *SUB = "-";
+char *MUL = "*";
+char *DIV = "/";
+char *LB = "(";
+char *RB = ")";
+char *EQ = "==";
+char *NOT_EQ = "!=";
+char *LARGE = "<";
+char *LARGE_EQ = "<=";
+char *SMALL = ">";
+char *SMALL_EQ = ">=";
+
 // エラーを報告するための関数
 // printfと同じ引数を取る
 void error(char *fmt, ...) {
@@ -72,18 +85,105 @@ Token *new_token(TokenKind kind, Token *cur, char *str) {
         return tok;
 }
 
-char *ADD = "+";
-char *SUB = "-";
-char *MUL = "*";
-char *DIV = "/";
-char *LB = "(";
-char *RB = ")";
-char *EQ = "==";
-char *NOT_EQ = "!=";
-char *LARGE = "<";
-char *LARGE_EQ = "<=";
-char *SMALL = ">";
-char *SMALL_EQ = ">=";
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs){
+        Node *node = calloc(1, sizeof(Node));
+        node->kind = kind;
+        node->lhs = lhs;
+        node->rhs = rhs;
+        return node;
+}
+
+Node *new_node_num(int val){
+        Node *node = calloc(1, sizeof(Node));
+        node->kind = ND_NUM;
+        node->val = val;
+        return node;
+}
+
+
+
+
+Node *expr(){
+        Node *node = equality();
+        return node;
+}
+
+Node *equality(){
+        Node *node = relational();
+
+        for (;;){
+                if(consume(EQ))
+                        node = new_node(ND_EQ, node, relational());
+                else if(consume(NOT_EQ))
+                        node = new_node(ND_NOT_EQ, node, relational());
+                else
+                        return node;
+        }
+}
+
+Node *relational(){
+        Node *node = add();
+
+        for (;;){
+                if(consume(LARGE))
+                        node = new_node(ND_LARGE, node, add());
+                else if(consume(SMALL))
+                        node = new_node(ND_LARGE, add(), node);
+                else if(consume(LARGE_EQ))
+                        node = new_node(ND_LARGE_EQ, node, add());
+                else if(consume(SMALL_EQ))
+                        node = new_node(ND_LARGE_EQ, add(), node);
+                else
+                        return node;
+        }
+}
+
+Node *add(){
+        Node *node = mul();
+
+        for (;;){
+                if(consume(ADD))
+                        node = new_node(ND_ADD, node, mul());
+                else if(consume(SUB))
+                        node = new_node(ND_SUB, node, mul());
+                else
+                        return node;
+
+        }
+}
+
+Node *mul(){
+        Node *node = unary();
+
+        for (;;){
+                if(consume(MUL))
+                        node = new_node(ND_MUL, node, unary());
+                else if(consume(DIV))
+                        node = new_node(ND_DIV, node, unary());
+                else
+                        return node;
+        }
+}
+
+Node *primary(){
+        // 次のトークンが"("なら、"(" expr ")"のはず
+        if(consume(LB)){
+                Node *node = expr();
+                expect(RB);
+                return node;
+        }
+
+        // そうでなければ数値のはず
+        return new_node_num(expect_number());
+}
+
+Node *unary() {
+        if (consume(ADD))
+                return primary();
+        if (consume(SUB))
+                return new_node(ND_SUB, new_node_num(0), primary());
+        return primary();
+}
 
 
 // 入力文字列pをトークナイズしてそれを返す
